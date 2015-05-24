f56721e25a789baa3ab6475754b05e108240b917
Andrew Lewis <nerf@judo.za.org>
2015-05-24 19:07:08 +0200

    Restructure plugin

--- plugins/auth/auth_dovecot.js
+++ plugins/auth/auth_dovecot.js
@@ -1,13 +1,8 @@
 // Auth against Dovecot SASL
 var sock = require('./line_socket');
-var utils = require('./utils');
-
-var LOGIN_STRING1 = 'VXNlcm5hbWU6'; //UserLogin: base64 coded
-var LOGIN_STRING2 = 'UGFzc3dvcmQ6'; //Password: base64 coded
 
 exports.register = function () {
     var plugin = this;
-    plugin.inherits('auth/auth_base');
     plugin.load_dovecot_ini();
 };
 
@@ -30,12 +25,14 @@ exports.load_dovecot_ini = function () {
 
 };
 
+exports.pool = {};
+
 exports.advertise_auth = function(next, connection, methods) {
         if (methods && methods.length > 1) {
            connection.capabilities.push('AUTH ' + methods.join(' '));
            connection.notes.allowed_auth_methods = methods;
         }
-        next();
+        return next();
 };
 
 exports.hook_capabilities = function (next, connection) {
@@ -43,42 +40,13 @@ exports.hook_capabilities = function (next, connection) {
     var methods = [];
 
     if (!server.notes.dovecot_auth_methods) {
-        plugin.get_dovecot_auth_methods(next, connection, plugin.advertise_auth);
+        plugin.get_methods(next, connection, plugin.advertise_auth);
     } else {
         plugin.advertise_auth(next, connection, server.notes.dovecot_auth_methods);
     }
 };
 
-exports.auth_plain = function(next, connection, params) {
-    return this.try_dovecot_auth(next, connection, params, 'PLAIN');
-};
-
-exports.auth_login = function(next, connection, params) {
-    var plugin = this;
-    if ((!connection.notes.auth_login_asked_login && params[0]) ||
-        (connection.notes.auth_login_asked_login && !connection.notes.auth_login_userlogin))
-    {
-        var login = params[0];
-        connection.respond(334, LOGIN_STRING2, function () {
-            connection.notes.auth_login_userlogin = login;
-            connection.notes.auth_login_asked_login = true;
-            return next(OK);
-        });
-        return;
-    }
-
-    if (connection.notes.auth_login_userlogin) {
-        connection.notes.auth_login_password = params[0];
-        return plugin.try_dovecot_auth(next, connection, params, 'LOGIN');
-    }
-
-    connection.respond(334, LOGIN_STRING1, function () {
-        connection.notes.auth_login_asked_login = true;
-        return next(OK);
-    });
-};
-
-exports.get_dovecot_auth_methods = function(next, connection, advertise_auth) {
+exports.get_methods = function(next, connection, advertise_auth) {
     var plugin = this;
     var version;
     var methods = [];
@@ -98,60 +66,70 @@ exports.get_dovecot_auth_methods = function(next, connection, advertise_auth) {
                if (version != 1.1) {
                    connection.logerror('Got wrong protocol version from Dovecot: ' + version);
                    methods = [];
-                   break;
                }
                server.notes.dovecot_auth_methods = methods;
+               socket.end();
                return advertise_auth(next, connection, methods);
-               break;
         }
     });
 };
 
-exports.try_dovecot_auth = function(next, connection, params, scheme) {
+exports.hook_unrecognized_command = function (next, connection, params) {
     var plugin = this;
-    var socket = new sock.Socket();
-    socket.connect(plugin.cfg.main.port, plugin.cfg.main.hostname);
+    if(params[0].toUpperCase() === 'AUTH' && params[1]) {
+        connection.notes.authenticating = true;
+    }
+    if (!connection.notes.authenticating) { return next(); }
+    if (params[1]) {
+        var split = params[1].split(' ');
+    } else {
+        var split = [];
+    }
+    if (plugin.pool[connection.uuid]) {
+        var socket = plugin.pool[connection.uuid];
+        if (!socket.writable) {
+            socket.connect(plugin.cfg.main.port, plugin.cfg.main.hostname);
+        }
+        if (params[0] != 'AUTH') {
+            socket.write('CONT\t1\t'+params[0]+'\n');
+            return next(OK);
+        }
+    } else {
+        var socket = new sock.Socket();
+        socket.connect(plugin.cfg.main.port, plugin.cfg.main.hostname);
+        plugin.pool[connection.uuid] = socket;
+    }
+
+    socket.on('end', function() {
+        delete(plugin.pool[connection.uuid]);
+    });
 
     socket.on('line', function(line) {
         var res = line.split('\t');
         switch(res[0].trim()) {
            case 'DONE':
                socket.write('VERSION\t1\t1\nCPID\t'+process.pid+'\n');
-               switch (scheme) {
-                   case 'PLAIN':
-                       socket.write('AUTH\t1\tPLAIN\tservice='+plugin.cfg.main.service+'\tsecured\tresp='+params+'\n');
-                       break;
-                   case 'LOGIN':
-                       socket.write('AUTH\t1\tLOGIN\tservice='+plugin.cfg.main.service+'\tsecured\tresp='+connection.notes.auth_login_userlogin+'\n');
-                       break;
-                   default:
-                       connection.logerror('No handler for scheme: ' + scheme);
-                       connection.respond(535, 'Cannot authenticate', function() {
-                           return next(OK);
-                       });
-                       break;
+               socket.write('AUTH\t1\t'+split[0]+'\tservice='+plugin.cfg.main.service+'\tsecured');
+               if(split.length === 2) {
+                   socket.write('\tresp='+split[1]);
                }
+               socket.write('\n');
                break;
            case 'CONT':
-               socket.write('CONT\t1\t' + connection.notes.auth_login_password + '\n');
+               connection.respond(334, res[2], function () {
+               });
                break;
            case 'OK':
                connection.respond(235, 'Authentication successful', function() {
-                   if (scheme == 'PLAIN') {
-                       var credentials = utils.unbase64(params[0]).split(/\0/);
-                       connection.notes.auth_user = credentials[1];
-                       connection.notes.auth_passwd = credentials[2];
-                   } else if (scheme == 'LOGIN') {
-                       connection.notes.auth_user = utils.unbase64(connection.notes.auth_login_userlogin);
-                       connection.notes.auth_passwd = utils.unbase64(connection.notes.auth_login_password);
-                   }
-                   return next(OK);
+                   connection.notes.authenticating = false;
+                   socket.end();
                });
                break;
            case 'FAIL':
                connection.respond(535, 'Authentication failed', function() {
+                   connection.notes.authenticating = false;
                    connection.reset_transaction(function() {
-                       return next(OK);
+                       socket.end();
                    });
                });
                break;
@@ -159,5 +137,5 @@ exports.try_dovecot_auth = function(next, connection, params, scheme) {
                break;
         }; 
     });
-
+    return next(OK);
 }
